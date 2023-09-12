6cb64f70818c091fa55a927586896684b1da3714
Joanna Zacirka <joanna.zacirka@trapezegroup.pl>
2023-09-12 12:26:12 +0200

    Upgrade to net6.0

--- SumLists/Solution.cs
+++ SumLists/Solution.cs
@@ -2,17 +2,19 @@
 You have two numbers represented by a linked list, where each node contains a single digit. 
 The digits are stored in reverse order, such that the 1’s digit is at the head of the list. 
 Write a function that adds the two numbers and returns the sum as a linked list.
-(You are not allowed to "cheat" and just convert the linked list to an integer.)
+(The method will be used to calculate sum of numbers bigger than Int64.MaxValue)
 
 EXAMPLE
  Input: (7->1->6) + (5->9->2). That is 617 + 295
  Output: 2->1->9. That is 912
 
 */
+using System;
 using System.Collections.Generic;
 
 namespace SumLists
 {
+    
     public static class Solution
     {
         public static LinkedList<int> SumLists(LinkedList<int> number1, LinkedList<int> number2)
